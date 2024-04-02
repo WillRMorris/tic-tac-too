d40413533ed4ac2a91fe0d8568370d1c9023e75c
WickerPrison <cobylax17@gmail.com>
2024-04-01 20:46:54 -0600

    added seeds

--- models/index.js
+++ models/index.js
@@ -2,10 +2,12 @@ const User = require('./User');
 const Frienship = require('./friendship')
 
 User.belongsToMany(User, {
+    as: 'target',
     through: Frienship,
     foreignKey: 'user_id'
 })
 User.belongsToMany(User, {
+    as: 'friend',
     through: Frienship,
     foreignKey: 'friend_id'
 })
