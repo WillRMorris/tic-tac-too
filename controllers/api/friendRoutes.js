00c40bc57b73eb6a8ae3ebd22a9d06465af40bc9
WillRMorris <153463826+WillRMorris@users.noreply.github.com>
2024-04-08 21:01:35 -0600

    added route to update friendhsip stats

--- controllers/api/friendRoutes.js
+++ controllers/api/friendRoutes.js
@@ -1,20 +1,20 @@
 const router = require('express').Router();
 const { query } = require('express');
-const {User, Friendship} = require('../../models');
+const { User, Friendship } = require('../../models');
 // api/friends route
 
 router.get('/:id', async (req, res) => {
   //finds all of a users friends includes user table
-  try{
+  try {
     const friendsData = await User.findAll({
-        where: {
-            id: req.params.id,
-          },
-          include:[{model: User, as: 'target'}, {model: User, as: 'friend'}]
+      where: {
+        id: req.params.id,
+      },
+      include: [{ model: User, as: 'target' }, { model: User, as: 'friend' }]
     })
     res.json(friendsData);
   }
-  catch(err){
+  catch (err) {
     res.status(400).json(err)
     console.log(err)
   }
@@ -22,42 +22,42 @@ router.get('/:id', async (req, res) => {
 
 router.get('/single/:userId/:friendId', async (req, res) => {
   //finds a specifc friendship between two users by their ids
-  try{
+  try {
     let friendsData = await User.findAll({
       where: {
-          id: req.params.userId,
+        id: req.params.userId,
       },
-      include:[{model: User, as: 'target'}, {model: User, as: 'friend'}]
+      include: [{ model: User, as: 'target' }, { model: User, as: 'friend' }]
     })
-      let friend = await specifier(friendsData, req.params.friendId)
+    let friend = await specifier(friendsData, req.params.friendId)
     res.json(friend);
   }
-  catch(err){
+  catch (err) {
     res.status(400).json(err)
 
   }
 });
 
-async function specifier(data, val){
+async function specifier(data, val) {
   console.log(data);
   let set = data;
   let array = set[0]._previousDataValues.target;
   console.log(array);
 
-  for (let i = 0; i< array.length; i++){
-      if(array[i].dataValues.id == val){
-          return array[i];
-      }
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].dataValues.id == val) {
+      return array[i];
+    }
   }
   return console.log('specifier failed');
-  
+
 }
 module.exports = router;
 
 //creates a friendship between a given friend and the current user
 router.post('/', async (req, res) => {
   const userId = req.session.user_id;
-  if (req.body.friend_id){
+  if (req.body.friend_id) {
     const newFriend = [
 
       {
@@ -69,18 +69,28 @@ router.post('/', async (req, res) => {
         friend_id: userId
       }
     ]
-    try{
+    try {
 
       const friendsData = await Friendship.bulkCreate(newFriend);
       res.json('friendship created');
-    } 
-    catch (err){
+    }
+    catch (err) {
       res.status(400).json(err);
     }
-  } else{
-res.status(500).json('no proper friend_id in Post');
+  } else {
+    res.status(500).json('no proper friend_id in Post');
   }
 })
 
 
 //updates a friendship's shared stats.
+router.put('/:friendId', async (req, res) => {
+  const friendship = await Friendship.update(req.body,
+    {
+      where: {
+        user_id: req.session.user_id,
+        friend_id: req.params.friendId
+      }
+    }
+  )
+})
